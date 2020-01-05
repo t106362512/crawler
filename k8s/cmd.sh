413fa10d9c466d4a5751c0e4076810c2abcac07e
t106362512 <t106362512@github.com>
2020-01-05 17:37:23 +0800

    add k8s yaml config :~

---
+++ k8s/cmd.sh
@@ -0,0 +1,17 @@
+az aks get-credentials --resource-group securityProject --name Elastic --subscription 79fba278-10c8-4888-affd-2bcf4c1f75d9
+kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=kube-system:kubernetes-dashboard
+
+kubectl apply -f https://download.elastic.co/downloads/eck/1.0.0-beta1/all-in-one.yaml 
+
+kubectl apply -f ./stepByStep/1.createElasticsearch.yml 
+
+#使用 kubectl get pods -w 確認 Elasticsearch 是否已建立完成
+kubectl get pods -w
+
+kubectl apply -f ./stepByStep/2.createKibana.yml
+kubectl apply -f ./stepByStep/3.runCustomContainer.yml
+kubectl apply -f ./stepByStep/4.runCustomContainer_multi.yml
+PASSWORD=$(kubectl get secret seelasticsearch-es-elastic-user -o=jsonpath='{.data.elastic}' | base64 --decode)
+# https://www.elastic.co/guide/en/cloud-on-k8s/current/k8s-quickstart.html#k8s_request_elasticsearch_access
+kubectl get service #找到port為9200的外部ip
+curl -u 'elastic:'${PASSWORD} -k 'https://{externaleIP}:9200'
